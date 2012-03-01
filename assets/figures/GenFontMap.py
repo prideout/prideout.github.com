95f6b4f752e9280192791419202240a08a71eb37
Philip Rideout <prideout@gmail.com>
2012-02-29 20:40:00 -0800

    some images for my next blog entry

--- assets/figures/GenFontMap.py
+++ assets/figures/GenFontMap.py
@@ -10,53 +10,45 @@ svgfile, pngfile = ("FontMap.svg", "FontMap.png")
 foreground = (0,0,0)
 background = (1,1,1)
 
-size = (1400*2, 108*8-40)
+def init_font(cr):
+    cr.select_font_face("Monospace", cairo.FONT_SLANT_NORMAL, cairo.FONT_WEIGHT_BOLD) # NORMAL)
+    cr.set_font_size(64.0)
+    return cr.font_extents()[:4]
+
+# Temporary surface to determine font metrics
+size = (64, 64)
 surface = cairo.SVGSurface(svgfile, *size)
 cr = cairo.Context(surface)
+extents = init_font(cr)
+print extents
+(x_bearing, y_bearing, width, height) = extents
+width = math.ceil(width) + 2;
+height = math.ceil(height + y_bearing) + 2;
 
+# Create the grid surface
+size = (width * 24, height * 4)
+surface = cairo.SVGSurface(svgfile, *size)
+cr = cairo.Context(surface)
+print init_font(cr)
 cr.set_source_rgb(*background)
 cr.rectangle(0, 0, size[0], size[1])
-#cr.fill()
-
-def text(str, x, y):
-    x_bearing, y_bearing, width, height = cr.text_extents(str)[:4]
-    cr.move_to(x - width / 2 - x_bearing, y - height / 2 - y_bearing)
-    cr.show_text(str)
-    return width, height
-
-cr.select_font_face("Monospace", cairo.FONT_SLANT_NORMAL, cairo.FONT_WEIGHT_BOLD) # NORMAL)
-cr.set_font_size(192)
-cr.set_source_rgb(*foreground)
-#width, height = text("Hellop", 128, 16)
-
-cr.set_source_rgb(*background)
-cr.rectangle(0, 0, 256, 32)
-#cr.fill()
+cr.fill()
 
 s = ""
 for i in xrange(ord(' '), 1 + ord('~')):
     s = s + chr(i)
 print s
 
-t1 = len(s)/4+1
-t2 = 2*len(s)/4+1
-t3 = 3*len(s)/4+1
-s1, s2, s3, s4 = s[:t1], s[t1:t2], s[t2:t3], s[t3:]
-
-x_bearing, y_bearing, width, height = cr.text_extents(s)[:4]
 cr.set_source_rgb(*foreground)
-cr.move_to(2, -y_bearing + 2)
-cr.show_text(s1)
-cr.move_to(2, -y_bearing + 2 + height)
-cr.show_text(s2)
-cr.move_to(2, -y_bearing + 2 + height*2)
-cr.show_text(s3)
-cr.move_to(2, -y_bearing + 2 + height*3)
-cr.show_text(s4)
+g = ord(' ') - 29 # No clue how this works.
+y = y_bearing
+for row in xrange(4):
+    x = 0
+    for col in xrange(24):
+        cr.show_glyphs([(g, x, y)])
+        g = g + 1
+    y = y + height + y_bearing
 
-width = width + x_bearing
-print width, height
-print width / len(s)
 
 surface.write_to_png(pngfile)
 surface = 0
