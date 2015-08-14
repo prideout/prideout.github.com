63925ac44caa0c7b2bdd937fb5925fce874ab392
Philip Rideout <philip@napofearth.com>
2015-08-13 21:02:57 -0700

    Add a "post" for heman.

--- assets/thumbnails/mask.py
+++ assets/thumbnails/mask.py
@@ -41,8 +41,8 @@ mask.load()
 
 sourceChannels = source.split()
 maskChannels = mask.split()
-#dest = Image.merge("RGBA", (sourceChannels[0:3]) + (maskChannels[3],))
-dest = Image.merge("LA", (sourceChannels[0],) + (maskChannels[3],))
+dest = Image.merge("RGBA", (sourceChannels[0:3]) + (maskChannels[3],))
+#dest = Image.merge("LA", (sourceChannels[0],) + (maskChannels[3],))
 
 masked = root.split('-')[0] + "-masked" + ext
 #import code; code.interact(local=locals()) # uncomment to break debug interactive
